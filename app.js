f5b0688342de538d136e8dcd03b4018622743f18
Abdy Fekadu <42736292+Abdy1@users.noreply.github.com>
2021-12-22 11:46:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,8 @@
   var headerBackgroundColor = "#f1f1f1;"
   var headerPadding = "20px 10px"
   var trackheader = 0;
+  //tracks the state of the program. if 0 its outside ready to add new component
+  var trackstate = 0;
 
 
   function commit(){
@@ -35,8 +37,16 @@
 
 function letsGo(event){
   var userinput = document.getElementById("myInput").value;
+
+  console.log(trackstate + "is the value of state if 0 its outside");
+
+  //make sure state is 0 before creating new element
+  if (trackstate == 0) {
+    console.log("i entered the if statement");
   switch(userinput) {
     case "header":
+      trackstate = 1;
+      console.log("now state is " + trackstate);
       trackheader = trackheader + 1;
       html.value += "\n<!--headerone"+trackheader+"-->\n\n<div class=\"header\">\n <a href=\"#default\" class=\"logo\"> CompanyLogo </a>\n <div class=\"header-right\">\n  <a class=\"active\" href=\"#home\">Home</a>\n  <a href=\"#contact\">Contact</a>\n  <a href=\"#about\">About</a>\n </div>\n</div>\n\n<!--headerone"+trackheader+"closed-->\n\n";
       if(DidCssHappenedHeader == 0){
@@ -56,7 +66,27 @@ function letsGo(event){
       commit();     
       break;
     default:
-  }
+  }}
+
+  //if state is 1 we know we are in header. so we let the user make modification of header
+  if (trackstate == 1) {
+    console.log("i entered the if statement");
+  switch(userinput) {
+    case "red":
+      console.log(headerBackgroundColor);
+      headerBackgroundColor = userinput;
+      console.log(headerBackgroundColor);
+    
+    commit();
+    break;
+    case "OTHER":
+      html.value += "<h1> YOU </h1>";
+  
+      commit();     
+      break;
+    default:
+  }}
+
   event.preventDefault();
 }
 form.addEventListener('submit', letsGo);
@@ -71,5 +101,5 @@ form.addEventListener('submit', letsGo);
 //to target and delete
 //var man = html.value.replace(  html.value.slice(html.value.indexOf("<!--start-->"),html.value.indexOf("<!--finish-->")+23),'');
 //html.value = man;
- //console.log(man);
+//console.log(man);
 
